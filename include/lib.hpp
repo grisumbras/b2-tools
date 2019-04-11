d8b91f705faf68c014f614c87dce7e3704e6545c
Архипов Дмитрий <arhipov_d@magnit.ru>
2019-04-11 16:21:04 +0300

    adds doxygen

---
+++ include/lib.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+
+/**
+ * @file lib.hpp
+ * @brief A file
+ */
+
+
+/// A class
+class C {
+public:
+  /// A constructor
+  C();
+
+  /// A method
+  void m();
+
+private:
+  int n;
+};
